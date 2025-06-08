4acca655cd11c3bbb123f00c21cb11b51a606b97
jmcada2503 <juanmiscj25@gmail.com>
2025-06-08 12:07:49 -0500

    init

---
+++ test.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def distance(point1, point2):
+    return np.sqrt(np.sum((point1 - point2)**2))
+
+
+points = np.array([
+    [0.13549547, 0.66202045],
+    [0.47046747, 0.02456593],
+    [0.34690738, 0.98025478],
+    [0.41461822, 0.32904476],
+    [0.34483581, 0.8041709],
+    [0.22544901, 0.19767214],
+    [0.59941989, 0.19204002],
+    [0.21255983, 0.63456189],
+    [0.72419519, 0.93991517],
+    [0.7139072, 0.07865282]
+])
+n_ants = 10
+n_iterations = 100
+alpha = 1
+beta = 1
+evaporation_rate = 0.5
+Q = 1
+
+n_points = len(points)
+pheromone = np.ones((n_points, n_points))
+best_path = []
+best_path_length = np.inf
+
+for _ in range(n_iterations):
+    paths = []
+    path_lengths = []
+
+    for ant in range(n_ants):
+        visited = np.array([False]*n_points)
+        current_point = np.random.randint(n_points)
+        visited[current_point] = True
+        path = [current_point]
+        path_length = 0
+
+        while False in visited:
+            unvisited = np.where(np.logical_not(visited))[0]
+            probabilities = np.zeros(len(unvisited))
+
+            for i, unvisited_point in enumerate(unvisited):
+                probabilities[i] = (
+                    pheromone[current_point, unvisited_point]**alpha /
+                    distance(
+                        points[current_point],
+                        points[unvisited_point]
+                    )**beta
+                )
+
+            probabilities /= np.sum(probabilities)
+
+            next_point = np.random.choice(unvisited, p=probabilities)
+            path.append(next_point)
+            path_length += distance(
+                points[current_point],
+                points[next_point]
+            )
+            visited[next_point] = True
+            current_point = next_point
+
+        paths.append(path)
+        path_lengths.append(path_length)
+
+        if path_length < best_path_length:
+            best_path = path
+            best_path_length = path_length
+
+    pheromone *= evaporation_rate
+
+    for path, path_length in zip(paths, path_lengths):
+        for i in range(n_points-1):
+            pheromone[path[i], path[i+1]] += Q/path_length
+        pheromone[path[-1], path[0]] += Q/path_length
+
+best_path_points = points[best_path]
+plt.scatter(points[:, 0], points[:, 1], c='r', marker='o')
+plt.plot(
+    best_path_points[:, 0],
+    best_path_points[:, 1],
+    c='g', linestyle='-', linewidth=2, marker='o'
+)
+
+plt.show()
